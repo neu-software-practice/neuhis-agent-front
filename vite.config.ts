2d441c601b818b797aebf1960c1ef0ff2e5a6685
Kirikaze Chiyuki <me@chyk.ink>
2026-07-01 21:38:21 +0800

    脱敏

--- vite.config.ts
+++ vite.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
     },
     proxy: {
       "/api": {
-        target: "http://***REMOVED***:8081",
+        target: "http://REDACTED:8081",
         changeOrigin: true,
       },
     },
